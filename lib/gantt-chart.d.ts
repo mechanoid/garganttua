4b1f3887d14b2965c0e843b2233d1a2842559ed4
Falk Hoppe <falkhoppe81@gmail.com>
2021-08-21 09:06:32 +0200

    remove build files from lib

--- lib/gantt-chart.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class GanttChart extends HTMLElement {
-    connectedCallback(): void;
-    provideConfigAsCSSProps(): void;
-    get days(): string;
-    get groups(): string;
-    get src(): string | null;
-}
--- lib/task-schedule.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class TaskSchedule extends HTMLElement {
-    connectedCallback(): void;
-    provideConfigAsCSSProps(): void;
-    get start(): string | null;
-    get end(): string | null;
-}
