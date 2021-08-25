601e8e9a4209b68066a4a41a96a6dafaa52b0ee8
Falk Hoppe <falkhoppe81@gmail.com>
2021-08-25 23:33:23 +0200

    validate task content for malicious js attr bindings

--- lib/task-schedule-content-element.ts
+++ lib/task-schedule-content-element.ts
@@ -1,13 +1,25 @@
 import { Task } from './task-list.js'
 
+// innerHTML does not execute script tags, but still allows people to bind
+// JS to event-attributes. We validate the content below for such attributes and
+// reject if any such string is included (also in the text).
+// We do explicitly no sanitization as this is doomed to fail.
+// There might be ways around that too, but unless people also inject JS code
+// in other places this is a good start.
+const JS_EVENT_ATTRIBUTES = Object.keys(window).filter(k => !k.indexOf('on'))
+
 export class TaskScheduleContentElement extends HTMLElement {
   task?: Task
 
   connectedCallback (): void {
     if (this.task?.content) {
-      console.log(atob(this.task.content))
+      const decoded = atob(this.task.content)
 
-      this.innerHTML = atob(this.task.content).toString()
+      if (JS_EVENT_ATTRIBUTES.some(attr => decoded.includes(attr))) {
+        throw new Error(`found malicious content in task: ${this.task?.description}`)
+      }
+
+      this.innerHTML = decoded
     }
   }
 
