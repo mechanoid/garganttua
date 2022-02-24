de27001636754e29321a695aab7d256a0c9d7f79
Falk Hoppe <falkhoppe81@gmail.com>
2022-02-24 17:38:57 +0100

    fix utf-8 strings in b64 encoded content parts

--- lib/task-schedule-content-element.ts
+++ lib/task-schedule-content-element.ts
@@ -1,4 +1,4 @@
-import { Task } from './task-list.js'
+import { Task } from "./task-list.js";
 
 // innerHTML does not execute script tags, but still allows people to bind
 // JS to event-attributes. We validate the content below for such attributes and
@@ -6,26 +6,32 @@ import { Task } from './task-list.js'
 // We do explicitly no sanitization as this is doomed to fail.
 // There might be ways around that too, but unless people also inject JS code
 // in other places this is a good start.
-const JS_EVENT_ATTRIBUTES = Object.keys(window).filter(k => !k.indexOf('on'))
+const JS_EVENT_ATTRIBUTES = Object.keys(window).filter((k) => !k.indexOf("on"));
 
 export class TaskScheduleContentElement extends HTMLElement {
-  task?: Task
+  task?: Task;
 
-  connectedCallback (): void {
+  connectedCallback(): void {
     if (this.task?.content) {
-      const decoded = atob(this.task.content)
+      const decoded = decodeURIComponent(
+        escape(window.atob(this.task.content)),
+      );
 
-      if (JS_EVENT_ATTRIBUTES.some(attr => decoded.includes(attr))) {
-        throw new Error(`found malicious content in task: ${this.task?.description}`)
+      if (JS_EVENT_ATTRIBUTES.some((attr) => decoded.includes(attr))) {
+        throw new Error(
+          `found malicious content in task: ${this.task?.description}`,
+        );
       }
 
-      this.innerHTML = decoded
+      this.innerHTML = decoded;
     }
   }
 
-  static build (task: Task): TaskScheduleContentElement {
-    const element = document.createElement('garganttua-task-schedule-content') as TaskScheduleContentElement
-    element.task = task
-    return element
+  static build(task: Task): TaskScheduleContentElement {
+    const element = document.createElement(
+      "garganttua-task-schedule-content",
+    ) as TaskScheduleContentElement;
+    element.task = task;
+    return element;
   }
 }
--- lib/task-schedule-element.ts
+++ lib/task-schedule-element.ts
@@ -1,150 +1,189 @@
-import parseISO from 'date-fns/parseISO'
-import differenceInDays from 'date-fns/differenceInDays'
-import formatISO from 'date-fns/formatISO'
+import parseISO from "date-fns/parseISO";
+import differenceInDays from "date-fns/differenceInDays";
+import formatISO from "date-fns/formatISO";
 
-import { GanttChartElement } from './gantt-chart-element.js'
-import { Task, stateValidation } from './task-list.js'
-import { TaskScheduleContentElement } from './task-schedule-content-element.js'
+import { GanttChartElement } from "./gantt-chart-element.js";
+import { stateValidation, Task } from "./task-list.js";
+import { TaskScheduleContentElement } from "./task-schedule-content-element.js";
 
-const dateToGridStartCol = (date: Date, startDate: Date, rangeLength: number):number => {
-  const dateDifference = differenceInDays(date, startDate)
+const dateToGridStartCol = (
+  date: Date,
+  startDate: Date,
+  rangeLength: number,
+): number => {
+  const dateDifference = differenceInDays(date, startDate);
   if (dateDifference < 0) {
-    return 0
+    return 0;
   } else if (dateDifference > rangeLength) {
-    return rangeLength
+    return rangeLength;
   }
 
-  return dateDifference
-}
+  return dateDifference;
+};
 
-const dateToGridEndCol = (date: Date, endDate: Date, rangeLength: number):number => {
-  const dateDifference = differenceInDays(endDate, date)
+const dateToGridEndCol = (
+  date: Date,
+  endDate: Date,
+  rangeLength: number,
+): number => {
+  const dateDifference = differenceInDays(endDate, date);
 
   if (dateDifference <= 0) { // date is later then largest date in grid, so we show it until the end
-    return rangeLength
+    return rangeLength;
   } else if (dateDifference < rangeLength) { // date is dateDifference away from the grid end
-    return rangeLength - dateDifference
+    return rangeLength - dateDifference;
   }
 
   // date is earlier then earliest date in grid, so we show it from col 0
-  return 0
-}
+  return 0;
+};
 
 export class TaskScheduleElement extends HTMLElement {
-  ganttChart?: GanttChartElement
-  task?:Task
-  private _startDate?: string | Date
-  private _endDate?: string | Date
-  private _start?: number
-  private _end?: number
-  private _state?: string
-  content?: TaskScheduleContentElement
+  ganttChart?: GanttChartElement;
+  task?: Task;
+  private _startDate?: string | Date;
+  private _endDate?: string | Date;
+  private _start?: number;
+  private _end?: number;
+  private _state?: string;
+  content?: TaskScheduleContentElement;
 
-  connectedCallback (): void {
-    this.ganttChart = this.closest('garganttua-gantt-chart') as GanttChartElement
+  connectedCallback(): void {
+    this.ganttChart = this.closest(
+      "garganttua-gantt-chart",
+    ) as GanttChartElement;
 
     if (this.task?.state) {
-      this.state = this.task?.state
+      this.state = this.task?.state;
     }
 
     if (this.task?.content) {
-      this.content = TaskScheduleContentElement.build(this.task)
-      this.classList.add('has-content')
-      this.appendChild(this.content)
+      this.content = TaskScheduleContentElement.build(this.task);
+      this.classList.add("has-content");
+      this.appendChild(this.content);
     }
 
-    this.provideConfigAsCSSProps()
+    this.provideConfigAsCSSProps();
   }
 
-  provideConfigAsCSSProps (): void {
+  provideConfigAsCSSProps(): void {
     if (this.start !== null && this.end !== null) {
-      this.style.setProperty('--schedule-start', (this.start + 2).toString())
-      this.style.setProperty('--schedule-end', (this.end).toString())
+      this.style.setProperty("--schedule-start", (this.start + 2).toString());
+      this.style.setProperty("--schedule-end", (this.end).toString());
     }
 
     if (this.state) {
-      this.style.setProperty('background-color', `var(--task-state-${this.state}-bg, var(--task-schedule-base-bg-col))`)
-      this.style.setProperty('color', `var(--task-state-${this.state}-ft, inherit)`)
+      this.style.setProperty(
+        "background-color",
+        `var(--task-state-${this.state}-bg, var(--task-schedule-base-bg-col))`,
+      );
+      this.style.setProperty(
+        "color",
+        `var(--task-state-${this.state}-ft, inherit)`,
+      );
     }
   }
 
-  get start (): number {
+  get start(): number {
     if (this._start) {
-      return this._start
+      return this._start;
     } else if (this.startDate) {
       if (this.ganttChart?.start && this.ganttChart?.end) {
-        this._start = dateToGridStartCol(this.startDate as Date, this.ganttChart.start, this.ganttChart.columnCount)
-        return this._start
+        this._start = dateToGridStartCol(
+          this.startDate as Date,
+          this.ganttChart.start,
+          this.ganttChart.columnCount,
+        );
+        return this._start;
       }
 
-      throw new Error('no start/end for gantt-chart defined')
+      throw new Error("no start/end for gantt-chart defined");
     }
 
-    throw new Error('no start date defined for task')
+    throw new Error("no start date defined for task");
   }
 
-  get end (): number {
+  get end(): number {
     if (this._end) {
-      return this._end
+      return this._end;
     } else if (this.endDate) {
       if (this.ganttChart?.start && this.ganttChart?.end) {
-        this._end = dateToGridEndCol(this.endDate as Date, this.ganttChart.end, this.ganttChart.columnCount)
-        return this._end
+        this._end = dateToGridEndCol(
+          this.endDate as Date,
+          this.ganttChart.end,
+          this.ganttChart.columnCount,
+        );
+        return this._end;
       }
 
-      throw new Error('no start/end for gantt-chart defined')
+      throw new Error("no start/end for gantt-chart defined");
     }
 
-    throw new Error('no start date defined for task')
+    throw new Error("no start date defined for task");
   }
 
-  get startDate (): string | Date {
-    return this._startDate as Date
+  get startDate(): string | Date {
+    return this._startDate as Date;
   }
 
-  set startDate (unparsed: string | Date) {
-    this._startDate = (unparsed instanceof Date) ? unparsed : parseISO(unparsed)
-    this.setAttribute('start', formatISO(this._startDate, { representation: 'date' }))
+  set startDate(unparsed: string | Date) {
+    this._startDate = (unparsed instanceof Date)
+      ? unparsed
+      : parseISO(unparsed);
+    this.setAttribute(
+      "start",
+      formatISO(this._startDate, { representation: "date" }),
+    );
   }
 
-  get endDate (): string | Date {
-    return this._endDate as Date
+  get endDate(): string | Date {
+    return this._endDate as Date;
   }
 
-  set endDate (unparsed: string | Date) {
-    this._endDate = (unparsed instanceof Date) ? unparsed : parseISO(unparsed)
-    this.setAttribute('end', formatISO(this._endDate, { representation: 'date' }))
+  set endDate(unparsed: string | Date) {
+    this._endDate = (unparsed instanceof Date) ? unparsed : parseISO(unparsed);
+    this.setAttribute(
+      "end",
+      formatISO(this._endDate, { representation: "date" }),
+    );
   }
 
-  get state (): string {
-    return this._state || ''
+  get state(): string {
+    return this._state || "";
   }
 
-  set state (state: string) {
+  set state(state: string) {
     if (state) {
-      const parsed = stateValidation.parse(state) as string
+      const parsed = stateValidation.parse(state) as string;
 
       if (parsed) {
-        this._state = parsed
-        this.setAttribute('state', this._state)
-        this.classList.add(`state-${this._state}`)
+        this._state = parsed;
+        this.setAttribute("state", this._state);
+        this.classList.add(`state-${this._state}`);
       }
     }
   }
 
-  static build (task: Task, withoutContent = null): TaskScheduleElement {
-    const element = document.createElement('garganttua-task-schedule') as TaskScheduleElement
-    const copy = Object.assign({}, task, { content: withoutContent ? null : task.content })
-    element.task = copy
+  static build(
+    task: Task,
+    withoutContent: boolean | null = null,
+  ): TaskScheduleElement {
+    const element = document.createElement(
+      "garganttua-task-schedule",
+    ) as TaskScheduleElement;
+    const copy = Object.assign({}, task, {
+      content: withoutContent ? null : task.content,
+    });
+    element.task = copy;
 
     if (task.start) {
-      element.startDate = task.start
+      element.startDate = task.start;
     }
 
     if (task.end) {
-      element.endDate = task.end
+      element.endDate = task.end;
     }
 
-    return element
+    return element;
   }
 }
